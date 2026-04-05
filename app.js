0886863de07f7fc54bb6fb619bee621c87e9d042
TumharaGitHubName <hassaanali994@gmail.com>
2026-04-05 23:12:21 +0500

    Add weather fetching functionality and styling for the Weather App

---
+++ app.js
@@ -0,0 +1,25 @@
+var weadBox = document.getElementById("weatherResult")
+var city = document.getElementById("cityInput")
+function getWeather() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?appid=392fc470c1ac8b42b2f40951a9a96cc4&q=${city.value}&units=metric`)
+        .then(function (result) {
+            return result.json()
+        })
+        .then(function (data) {
+            if (data.cod == "404") {
+                weadBox.innerHTML = `<h1>${data.message}</h1>`
+                return;
+            }
+            weadBox.innerHTML = `
+                <h2>${data.name}, ${data.sys.country}</h2>
+                <h3>${data.main.temp}°C</h3>
+                <h3>${data.main.humidity}%</h3>
+                <p>${data.weather[0].description}</p>
+            `
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+    city.value = ""
+}
