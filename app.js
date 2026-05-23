bda1bafeb9a9fc234782a1924fc71f4650e311fa
Muhammad Kaif <kaifattari0123@gmail.com>
2026-05-23 22:55:04 +0500

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+ async function getData() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=392fc470c1ac8b42b2f40951a9a96cc4&q=karachi&units=metric`);
+    const data = await response.json();
+    console.log(data);
+    return data;x   
+}
+
 var weadBox = document.getElementById("weatherResult")
+var taytaty = document.getElementById("taytay")
+getData().then(data => {
+    taytaty.innerHTML = ` <h2>${data.name}, ${data.sys.country}</h2>
+                <h3>${data.main.temp}°C</h3>
+                <h3>${data.main.humidity}%</h3>
+                <h4>${data.weather[0].description.toUpperCase()}</h4>`
+});
 var city = document.getElementById("cityInput")
 function getWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?appid=392fc470c1ac8b42b2f40951a9a96cc4&q=${city.value}&units=metric`)
